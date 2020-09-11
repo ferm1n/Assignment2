62238367ea1196761502afca48959ec047347be9
ferm1n <fgicangao25@gmail.com>
2020-09-10 22:49:11 -0700

    assignment2

---
+++ Assignment2.js
@@ -0,0 +1,49 @@
+const products = [
+    {
+        id: 1,
+        name: 'lenovo Phone',
+        price: 10400,
+    },
+    {
+        id: 2,
+        name: 'iphone X',
+        price: 74300,
+    },
+    {
+        id: 3,
+        name: 'Mouse',
+        price: 800,
+    },
+    {
+        id: 4,
+        name: 'Keyboard',
+        price: 1800,
+    },
+    {
+        id: 5,
+        name: 'Sony Headset',
+        price: 19500,
+    },
+    {
+        id: 6,
+        name:'Acer Laptop',
+        price: 25400,
+    },
+  ];
+  const reservedProducts = [
+    {
+        id: 2,
+        name: 'iphone X',
+        price: 74300,
+    },
+    {
+        id: 5,
+        name: 'Sony Headset',
+        price: 19500,
+    },
+  ];
+  const productList = products.map((prod) => prod.id)
+  
+  const Prod = products.filter((prod) => prod.id )
+  
+  console.log(products)
